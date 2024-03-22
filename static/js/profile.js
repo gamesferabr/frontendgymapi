1f724895710bff606c8a6c3a4701f404d2b870da
gamesferabr <gabriel@timepix.com.br>
2024-03-22 11:48:07 -0700

    modified:   static/js/profile.js

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,4 +1,57 @@
-document.addEventListener('DOMContentLoaded', function () {
+async function refreshToken() {
+    let refreshToken = localStorage.getItem('refresh_token');
+    try{
+        let response = await fetch(`https://gym-api-930w.onrender.com/api/auth/token/refresh`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ refresh: refreshToken }),
+        });
+
+        if (response.ok) {
+            let data = await response.json();
+            localStorage.setItem('access_token', data.access);
+            console.log('Token refreshed successfully.');} 
+            
+            else {
+                console.error('Erro ao atualizar o token.');
+                localStorage.removeItem('access_token');
+                localStorage.removeItem('refresh_token');
+        
+                window.location.href = 'login.html';
+                return false; // Falha na atualização do token
+            }
+    }
+
+    catch (error) {
+        console.error('Erro ao atualizar o token:', error);
+        window.location.href = 'login.html';
+    }
+
+}
+
+async function checkAndRefreshToken() {
+    let accessToken = localStorage.getItem('access_token');
+    if (!accessToken) {
+        // window.location.href = 'login.html';
+        return;
+    }
+
+    // Decodifica o token para verificar a expiração
+    let decodedToken = jwt_decode(accessToken);
+    let currentTime = Date.now() / 1000; // Converte para segundos
+
+    if (decodedToken.exp < currentTime) {
+        // Token expirado, tenta renovar
+        await refreshToken();
+    }
+}
+
+document.addEventListener('DOMContentLoaded', async function () {
+
+    await checkAndRefreshToken();
+    
     // Recupera os valores do localStorage
     const username = localStorage.getItem('username');
     const lastName = localStorage.getItem('last_name');
